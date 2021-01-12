299cbb4056b461511ba507ed9d3273d8afdf9646
hamdi <hamdi.rahal@gmail.com>
2021-01-12 23:23:45 +0100

    update hooks

--- src/Newtodo.js
+++ src/Newtodo.js
@@ -1,36 +1,51 @@
-import React from "react";
+import React, { useState } from "react";
 import { addTodo } from "./action";
-import { connect } from "react-redux";
-
-class Newtodo extends React.Component {
-  state = {
-    newtodo: ""
+import { useDispatch } from "react-redux";
+const Newtodo = () => {
+  const [newtodo, setNewtodo] = useState("");
+  const [inputBorder, setinputBorder] = useState(false);
+  const dispatch = useDispatch();
+  const handleAddnewTodo = () => {
+    if (newtodo) {
+      dispatch(
+        addTodo({
+          isComplete: false,
+          todo: newtodo,
+          id: Math.random(),
+        })
+      );
+      setNewtodo("");
+    } else {
+      setinputBorder(true);
+    }
   };
-  render() {
-    return (
-      <div className="container request">
-        <div className="col request-content">
-          <h1>To-Do App!</h1>
-          <p>Add New To-Do</p>
-          <input
-            type="text"
-            className="form-control input-val"
-            placeholder="Enter new task"
-            onChange={e => this.setState({ newtodo: e.target.value })}
-            value={this.state.newtodo}
-          />
-          <button
-            type="button"
-            id="add-button"
-            className="btn btn-primary"
-            onClick={()=>{this.props.addTodo({isComplete:false,todo:this.state.newtodo,id:Math.random()});this.setState({newtodo:""})}}
-          >
-            Add
-          </button>
-        </div>
+  return (
+    <div className="container request">
+      <div className="col request-content">
+        <h1>To-Do App!</h1>
+        <p>Add New To-Do</p>
+        <input
+          type="text"
+          style={{ border: inputBorder ? "4px solid red" : "none" }}
+          className="form-control input-val"
+          placeholder="Enter new task"
+          onChange={(e) => {
+            setNewtodo(e.target.value);
+            setinputBorder(false);
+          }}
+          value={newtodo}
+        />
+        <button
+          type="button"
+          id="add-button"
+          className="btn btn-primary"
+          onClick={handleAddnewTodo}
+        >
+          Add
+        </button>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
-export default connect(null, { addTodo })(Newtodo);
+export default Newtodo;
--- src/todolist.js
+++ src/todolist.js
@@ -1,21 +1,28 @@
 import React from "react";
-import { deleteTodo, updateTodo, completeTodo,updateTodoRequest } from "./action";
-import { connect } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  deleteTodo,
+  updateTodo,
+  completeTodo,
+  updateTodoRequest,
+} from "./action";
+const Todolist = (props) => {
+  const dispatch = useDispatch();
+  const todolist = useSelector((state) => state);
 
-const todolist = props => {
   return (
     <div className="container response-content">
       <div className="response-title">
         <h3>Lets get some work done!</h3>
         <hr style={{ width: "100px" }} />
       </div>
-      {props.todolist.map((el, i) => (
+      {todolist.map((el, i) => (
         <div className="response" key={i}>
           <div className="d-flex w-100 pt-3">
             <button
               className="btn btn-outline-primary mr-3"
               onClick={() => {
-                props.completeTodo(el.id);
+                dispatch(completeTodo(el.id));
               }}
             >
               {el.isComplete ? "Undo" : "Complete"}
@@ -23,32 +30,39 @@ const todolist = props => {
             <button
               className="btn btn-outline-primary mx-3"
               onClick={() => {
-                props.deleteTodo(el.id);
+                dispatch(deleteTodo(el.id));
               }}
             >
               Remove
             </button>
-            <button className="btn btn-outline-primary mx-3" onClick={() => {props.updateTodoRequest(el.id)}}>
+            <button
+              className="btn btn-outline-primary mx-3"
+              onClick={() => {
+                dispatch(updateTodoRequest(el.id));
+              }}
+            >
               Update
             </button>
-            {
-              el.request? 
-              <input type="text" onKeyUp={e=>e.key==="Enter" &&props.updateTodo({...el,todo:e.target.value})}/>: 
-              <h4 style={{ textDecoration: el.isComplete ? "line-through" : "" }}>
-              {el.todo}
-            </h4>
-            }
-           
+            {el.request ? (
+              <input
+                type="text"
+                onKeyUp={(e) =>
+                  e.key === "Enter" &&
+                  dispatch(updateTodo({ ...el, todo: e.target.value }))
+                }
+              />
+            ) : (
+              <h4
+                style={{ textDecoration: el.isComplete ? "line-through" : "" }}
+              >
+                {el.todo}
+              </h4>
+            )}
           </div>
         </div>
       ))}
     </div>
   );
 };
-const mapStateToProps = state => ({ todolist: state });
-export default connect(mapStateToProps, {
-  deleteTodo,
-  updateTodo,
-  completeTodo,
-  updateTodoRequest
-})(todolist);
+
+export default Todolist;
